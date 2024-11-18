c1ab369c7c085c15db81f39d0ffa574bcf73cce1
kubko <kubko@redacted.finance>
2024-11-18 23:05:40 +0000

    feat: bootcamp list + landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,39 @@
-'use client';
+"use client";
 
-import { useAccount } from 'wagmi'
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { useIsAdmin } from './hooks/useIsAdmin';
-import { useIsManager } from './hooks/useIsManager';
-import { BootcampList } from '@/components/BootcampList';
-import Link from 'next/link';
+import { useAccount } from "wagmi";
+import { useIsAdmin } from "./hooks/useIsAdmin";
+import { useIsManager } from "./hooks/useIsManager";
+import { BootcampList } from "@/components/BootcampList";
+import Link from "next/link";
 
 export default function Home() {
   const account = useAccount();
-  const {isAdmin} = useIsAdmin();
-  const {isManager} = useIsManager();
+  const { isAdmin } = useIsAdmin();
+  const { isManager } = useIsManager();
 
-  if (account.isConnected || isAdmin || isManager) {
-    return (
-      <div className="flex flex-col gap-6 justify-center items-center">
-        <h1 className="text-4xl font-bold mb-4">Encode Bootcamp Deposits</h1>
-        <ConnectButton />
-        <div className='flex flex-row gap-6'>
-        {isAdmin && (
-          <Link href="/admin">
-            <div className="flex p-4 bg-rose-500 text-white rounded-md">
-              Admin Page
-            </div>
-          </Link>
-        )}
-        {isManager && (
-          <Link href="/manager">
-            <div className="flex p-4 bg-rose-500 text-white rounded-md">
-              Manager Page
-            </div>
-          </Link>
-        )}
-        </div>
-        <BootcampList />
-      </div>
-    );
-  }
-  else {
-    return (
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-bold mb-4">Encode Bootcamp Deposits</h1>
-        <ConnectButton />
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col gap-6 justify-center items-center">
+      {(account.isConnected || isAdmin || isManager) && (
+        <>
+          <div className="flex flex-row gap-3 w-[50%] pb-8 border-b">
+            {isAdmin && (
+              <Link href="/admin">
+                <div className="flex p-4 bg-blue-500 text-white rounded-md">
+                  Admin Page
+                </div>
+              </Link>
+            )}
+            {isManager && (
+              <Link href="/manager">
+                <div className="flex p-4 bg-blue-500 text-white rounded-md">
+                  Manager Page
+                </div>
+              </Link>
+            )}
+          </div>
+        </>
+      )}
+      <BootcampList />
+    </div>
+  );
 }
--- src/components/BootcampList.tsx
+++ src/components/BootcampList.tsx
@@ -6,14 +6,13 @@ import {
   Bootcamp,
   fetchBootcampDetails,
 } from "@/app/queries/BootcampFactory/fetchBootcampDetails";
-import { useAccount } from "wagmi";
+import Image from "next/image";
 
 export const BootcampList = () => {
   const [bootcamps, setBootcamps] = useState<Bootcamp[] | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [hydrated, setHydrated] = useState(false);
-  const { isConnected } = useAccount();
 
   const fetchBootcamps = async () => {
     setLoading(true);
@@ -39,9 +38,7 @@ export const BootcampList = () => {
 
   return (
     <div className="flex flex-col w-[50%]">
-      {!isConnected ? (
-        <div>Connect your wallet to view bootcamps</div>
-      ) : loading ? (
+      {loading ? (
         <div>Loading...</div>
       ) : error ? (
         <div>Error: {error}</div>
@@ -49,24 +46,45 @@ export const BootcampList = () => {
         <div>No bootcamps found</div>
       ) : (
         <>
-          <h3>all bootcamps:</h3>
-          <div className="flex flex-col gap-2 justify-between">
-            {bootcamps.map((bootcamp) => {
+          <div className="flex flex-row gap-2">
+            {bootcamps.map((bootcamp, i) => {
+              let name;
+              let imgSrc;
+              if (i === 0) {
+                name = "Advanced Solidity Bootcamp";
+                imgSrc = "/advancedSol.png";
+              } else if (i === 1) {
+                name = "ZK and Scaling Bootcamp";
+                imgSrc = "/zkScaling.png";
+              } else {
+                name = "EVM Bootcamp";
+                imgSrc = "/evm.png";
+              }
               return (
-                <div key={bootcamp.id}>
+                <div key={i}>
                   <Link href={`bootcamp-detail?id=${bootcamp.id}`}>
-                    <div className="flex p-8 border-4 border-black/60">
-                      {bootcamp.id} - {bootcamp.bootcampAddress}
+                    <div className="flex flex-col p-8 border-4 border-black/60 w-[265px] h-[340px] gap-1 rounded-md">
+                      <Image
+                        src={imgSrc}
+                        alt="Bootcamp Image"
+                        width={230}
+                        height={158}
+                        className="mb-5"
+                      />
+                      <b>{name}</b>
+                      <p>
+                        Start:{" "}
+                        {new Date(1732492800 * 1000).toLocaleDateString()}
+                      </p>
+                      <div className="flex bg-blue-500 rounded-full p-2 text-white text-center justify-center items-center mt-2">
+                        View More
+                      </div>
                     </div>
                   </Link>
                 </div>
               );
             })}
           </div>
-          <h3>currently running bootcamps:</h3>
-          todo: based on the start time and deadline
-          <h3>finished bootcamps:</h3>
-          todo: based on the start time and deadline
         </>
       )}
     </div>
