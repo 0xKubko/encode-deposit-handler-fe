5893da95708e068b9bb5b82e4124300abf525312
kubko <kubko@redacted.finance>
2024-11-18 23:50:20 +0000

    feat: admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,21 +4,21 @@ import { Button } from "@radix-ui/themes";
 import { useState } from "react";
 import { useIsAdmin } from "../hooks/useIsAdmin";
 import { blockExplorer } from "../const";
-import { addManager, removeManager } from "../queries/BootcampFactory/addManager";
+import {
+  addManager,
+  removeManager,
+} from "../queries/BootcampFactory/addManager";
 
 export default function Admin() {
-  const {isAdmin} = useIsAdmin();
+  const { isAdmin } = useIsAdmin();
   const [managerAddress, setManagerAddress] = useState("");
   const [tx, setTx] = useState<`0x${string}` | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  // todo: fetch this from backend as there is no list on-chain
-  const currentManagers = ["0x32776d56FcC46978549f18371112C90233210aC8", "0xC6CbDd7D90458c5e1003DdE243bF1561efAeE516"];
-
   const handleAddManager = async () => {
     setError(null);
     setTx(null);
-    const result = await addManager(managerAddress as `0x${string}`)
+    const result = await addManager(managerAddress as `0x${string}`);
     if (result instanceof Error) {
       console.error(result);
       setError(result.message);
@@ -40,21 +40,16 @@ export default function Admin() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Admin Page</h1>
+    <div className="flex flex-col items-center w-[30%] min-w-[500px] justify-center font-[family-name:var(--font-geist-sans)] m-auto">
+      <h1 className="text-3xl font-bold mb-5">Admin Page</h1>
       {!isAdmin ? (
         <p>You are not an admin</p>
       ) : (
         <>
           <p>Here, you can add and remove bootcamp managers.</p>
 
-          <h2>Current Managers</h2>
-          {currentManagers.map((manager) => (
-            <div key={manager}>{manager}</div>
-          ))}
-
           {/* Input Form for Adding a Manager */}
-          <div className="flex flex-col gap-4 w-full max-w-md">
+          <div className="flex flex-col w-[100%] gap-4 mt-4">
             <label className="flex flex-col">
               <span>Manager Address:</span>
               <input
@@ -65,10 +60,17 @@ export default function Admin() {
                 className="border rounded px-3 py-2"
               />
             </label>
-
-            <Button onClick={handleAddManager}>Add Manager</Button>
-            <Button onClick={handleRemoveManager}>Remove Manager</Button>
-            {tx !== null && <a href={`${blockExplorer}/tx/${tx}`}>See on the explorer!</a>}
+            <div className="flex flex-row gap-4 justify-between">
+              <Button onClick={handleAddManager} className="w-[49%]">
+                Add Manager
+              </Button>
+              <Button onClick={handleRemoveManager} className="w-[49%]">
+                Remove Manager
+              </Button>
+            </div>
+            {tx !== null && (
+              <a href={`${blockExplorer}/tx/${tx}`}>See on the explorer!</a>
+            )}
             {error && <p className="text-red-500">{error}</p>}
           </div>
         </>
