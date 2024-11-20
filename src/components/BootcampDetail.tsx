cf3870d134013cf19b3169a72d414b2c6f3294e4
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-11-20 17:57:07 +0100

    🐛 [FIX] Re-rendering

--- src/components/BootcampDetail.tsx
+++ src/components/BootcampDetail.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import { useAccount } from "wagmi";
-import { useState } from "react";
+import { useMemo } from "react";
 import Image from "next/image";
 import { Address } from "viem";
 import { useBootcampDetails } from "@/app/hooks/useBootcampDetails";
@@ -12,27 +12,12 @@ import { BootcampManagerActions } from "./BootcampActions/BootcampManagerActions
 
 export function BootcampDetail() {
   const { address: walletAddress, isConnected } = useAccount();
-  const [error, setError] = useState<string | null>(null);
   const params = useSearchParams();
   const address = params.get("address");
   const { bootcamp, isLoading } = useBootcampDetails(address as Address);
 
-
-  if (!address) {
-    setError("No address found");
-  }
-  if (bootcamp instanceof Error) {
-    setError(bootcamp.message);
-    return <div>Error: {error}</div>;
-  }
-
-  if (!bootcamp) {
-    return <div>No bootcamp found</div>;
-  }
-
-  const bootcampName = bootcamp.name;
   let bootcampImgSrc;
-  switch (bootcampName) {
+  switch (bootcamp?.name) {
     case "Advanced Solidity Bootcamp":
       bootcampImgSrc = "/advancedSol.png";
       break;
@@ -47,56 +32,55 @@ export function BootcampDetail() {
       break;
   }
 
+
+  const isBootcampError = bootcamp instanceof Error;
   return (
     <div className="flex flex-col items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
-      {isLoading ? (
-        <div>Loading...</div>)
-      : error ? (
-        <div>Error: {error}</div>
-      ) : (
-        <div className="flex flex-row min-w-[1000px] w-[80%] mb-8 justify-between">
-          <div className="flex flex-col gap-3 w-[50%]">
-            <h1 className="text-3xl font-bold">{bootcampName}</h1>
-            <div className="flex items-center border-4 border-rose-900 p-5 rounded-md bg-rose-600 text-white my-3 justify-center">
-              Make sure to be accepted, if you are not and deposit, it will be
-              lost
-            </div>
-            <div className="flex flex-row gap-4 items-center justify-between">
-              <div className="flex flex-col gap-2">
-                <b>Start:</b>
-                <p>{bootcamp.bootcampStartTime.toDateString()}</p>
+      {isLoading ? (<div>Loading...</div>)
+        : (bootcamp && !isBootcampError && (
+          <div className="flex flex-row min-w-[1000px] w-[80%] mb-8 justify-between">
+            <div className="flex flex-col gap-3 w-[50%]">
+              <h1 className="text-3xl font-bold">{bootcamp.name}</h1>
+              <div className="flex items-center border-4 border-rose-900 p-5 rounded-md bg-rose-600 text-white my-3 justify-center">
+                Make sure to be accepted, if you are not and deposit, it will be
+                lost
               </div>
-              <div className="flex flex-col gap-2">
-                <b>Deadline:</b>
-                {/* TODO: handle deadline from the contract */}
-                <p>{bootcamp.bootcampDeadline.toDateString()}</p>
-                
-              </div>
-            </div>
-            <div className="flex flex-col gap-4">
-              <b>Bootcamp Address:</b> {address}
-            </div>
-            {isConnected && walletAddress && (
-              <>
-                <div className="flex flex-col border-t pt-5 gap-4">
-                  <b>Actions:</b>
-                  <DepositWithdraw bootcamp={bootcamp} walletAddress={walletAddress} />
+              <div className="flex flex-row gap-4 items-center justify-between">
+                <div className="flex flex-col gap-2">
+                  <b>Start:</b>
+                  <p>{bootcamp.bootcampStartTime.toDateString()}</p>
                 </div>
-                <BootcampManagerActions bootcamp={bootcamp} walletAddress={walletAddress} />
-              </>
-            )}
+                <div className="flex flex-col gap-2">
+                  <b>Deadline:</b>
+                  {/* TODO: handle deadline from the contract */}
+                  <p>{bootcamp.bootcampDeadline.toDateString()}</p>
+
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <b>Bootcamp Address:</b> {address}
+              </div>
+              {isConnected && walletAddress && (
+                <div>
+                  <div className="flex flex-col border-t pt-5 gap-4">
+                    <b>Actions:</b>
+                    <DepositWithdraw bootcamp={bootcamp} walletAddress={walletAddress} />
+                  </div>
+                  <BootcampManagerActions bootcamp={bootcamp} walletAddress={walletAddress} />
+                </div>
+              )}
+            </div>
+            <div className="flex items-center justify-center">
+              <Image
+                src={bootcampImgSrc}
+                alt="Bootcamp Image"
+                width={676}
+                height={466}
+                className="mb-5"
+              />
+            </div>
           </div>
-          <div className="flex items-center justify-center">
-            <Image
-              src={bootcampImgSrc}
-              alt="Bootcamp Image"
-              width={676}
-              height={466}
-              className="mb-5"
-            />
-          </div>
-        </div>
-      )}
+        ))}
     </div>
   );
 }
