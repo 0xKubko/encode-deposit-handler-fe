4bf5e82b2d748c49841d58acd24a2cc6e47ad6fa
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-11-20 01:07:04 +0100

    🐛 [FIX] Config for private RPC

--- src/configs/wagmi.ts
+++ src/configs/wagmi.ts
@@ -5,6 +5,10 @@ import {
   polygonAmoy,
 } from 'wagmi/chains';
 
+let transports: { [key: number]: ReturnType<typeof http> } = {};
+transports[polygon.id] = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY ? http(`https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`) : http();
+transports[polygonAmoy.id] = process.env.NEXT_PUBLIC_AMOY_ALCHEMY_API_KEY ? http(`https://polygon-amoy.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_AMOY_ALCHEMY_API_KEY}`) : http();
+
 export const config = getDefaultConfig({
   appName: 'Encode Bootcamp Deposits',
   projectId: '347da63bcb1904ec2a5602ce20e207dc',
@@ -12,8 +16,5 @@ export const config = getDefaultConfig({
     polygon,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS ? [polygonAmoy] : []),
   ],
-  transports: {
-    [polygon.id]: http(process.env.NEXT_PUBLIC_ALCHEMY_API_KEY),
-    [polygonAmoy.id]: http(process.env.NEXT_PUBLIC_AMOY_ALCHEMY_API_KEY),
-  },
+  transports: transports,
 });
